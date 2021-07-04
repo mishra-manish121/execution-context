13fc702ec69d7eae541aa2c6da784e792116589d
aman.s <aman.s@yourpayroll.co.nz>
2021-07-04 08:15:40 +0530

    done

---
+++ execution/execution context9.js
@@ -0,0 +1,14 @@
+function a() {
+  var ab = 2;
+  console.log(ab);
+
+  function b() {
+    // var ab;
+    console.log(ab);
+  }
+  b();
+  console.log(ab);
+}
+var ab = 10;
+a();
+console.log(ab);
